c6141eec128b4ef7dd3188d8f9b36cf9e7f16c80
Andreas Rex <rex199@gmail.com>
2019-10-10 13:59:36 +0200

    Uploading progress 10/10/19

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+    public static PApplet processing;
+
+    Rabbit rabbitOne = new Rabbit(10,10);
+    public static void main (String[] args) {
+
+        PApplet.main("Main", args);
+
+    }
+
+    public void settings () {
+
+        size(500,500);
+    }
+
+    public void setup() {
+        processing = this;
+    }
+
+    public void draw()
+    {
+        background(200);
+        rabbitOne.drawRabbit();
+    }
+}
---
+++ src/Rabbit.java
@@ -0,0 +1,68 @@
+import java.util.Random;
+
+public class Rabbit {
+
+    private int x = 0;
+    private int y = 0;
+    private int movementSpeed = 3;
+    public Random randomNumber = new Random();
+    public Rabbit(int x, int y)
+    {
+        this.setX(x);
+        this.setY(y);
+    }
+
+    public void drawRabbit() {
+
+        Main.processing.rect(getX(), getY(),10,10);
+        moveDirection();
+    }
+
+    public void moveDirection() {
+
+        int a =randomNumber.nextInt(5);
+
+        switch (a){
+
+            case 0:
+                if(x < Main.processing.width){
+                    this.x += movementSpeed;
+                }
+                break;
+            case 1:
+                if ( x > 0){
+                    this.x -= movementSpeed;
+                }
+                break;
+            case 2:
+                if (y < Main.processing.height -3){
+                    this.y += movementSpeed;
+                }
+                break;
+            case 3:
+                if (y > 3 ){
+                    this.y -= movementSpeed;
+                }
+                break;
+        }
+
+    }
+
+
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
