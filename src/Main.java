ab0147dc4658237e2d34ac799f5a707fb61bc077
Ec2IsDifficult <rasser.arm@live.dk>
2019-10-21 12:42:38 +0200

    Rabbit objects put in an ArrayList called arrayOfRabbits New Rabbit function have been created. New rabbits are created when the old rabbits are in a certain distance of each other. (cherry picked from commit db2b4844511cf0139c69d56b59e3d7b01ad0ecf1)

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,22 @@
 import processing.core.PApplet;
 
+import java.util.Random;
 import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
 public class Main extends PApplet {
-    //public PApplet p; //Just called P
+    public PApplet p; //Just called P
 
-    public ArrayList<Rabbit> arrayOfRabbits = new ArrayList<>(2);
+    int savedTime = millis();
+    int savedTime1 = millis();
+    int matingTimeRequired = 5000;
+    int matureTimerRequired = 5000;
 
-    public ArrayList<Rabbit> getArrayOfRabbits() {
-        arrayOfRabbits.add(new Rabbit(this, 10, 10));
-        arrayOfRabbits.add(new Rabbit(this, 400, 400));
+    //Making Rabbits via ArrayList.
+    public ArrayList<Rabbit> arrayOfRabbits = new ArrayList<>();
+
+    public ArrayList<Rabbit> getArrayOfRabbits(PApplet p, int x, int y) {
+        arrayOfRabbits.add(new Rabbit(p,x,y));
         return arrayOfRabbits;
     }
 
@@ -20,43 +26,67 @@ public class Main extends PApplet {
     Rabbit rabbitFour = new Rabbit(this, 70,70);
     Rabbit rabbitFive = new Rabbit(this,100,100);
     Rabbit rabbitSix = new Rabbit(this,200,200);*/
-
     /*public float viewDistanceFloat() {
 
         for (int i = 0; i < 2; i++) {
-
-
-
         }
     }*/
 
 
     public static void main(String[] args) {
-
         PApplet.main("Main", args);
-
     }
 
     public void settings() {
-
         size(800, 800);
     }
 
     public void setup() {
-
+        //Setting up the rabbits in the ArrayList.
+        getArrayOfRabbits(this,10,10);
+        getArrayOfRabbits(this,400,400);
     }
 
     public void draw() {
+
         background(200);
 
-        getArrayOfRabbits().get(1).drawRabbit();
-        getArrayOfRabbits().get(2).drawRabbit();
+        //New rabbit function that creates rabbits,
+        newRabbit();
 
-    }
+        //Running through the ArrayList to draw the rabbits.
+        for (int i = 0; i < arrayOfRabbits.size(); i++) {
+            arrayOfRabbits.get(i).drawRabbit();
+        }
+
+
+
         /*rabbitOne.drawRabbit();
         rabbitTwo.drawRabbit();
         rabbitThree.drawRabbit();
         rabbitFour.drawRabbit();
         rabbitFive.drawRabbit();
         rabbitSix.drawRabbit();*/
+
+    }
+
+    public void newRabbit() {
+
+        //Getting the coordinates from two rabbits
+        int x1 = arrayOfRabbits.get(0).getX();
+        int y1 = arrayOfRabbits.get(0).getY();
+        int x2 = arrayOfRabbits.get(1).getX();
+        int y2 = arrayOfRabbits.get(1).getY();
+
+        //Making a float using coordinates from rabbits.
+        float dist = dist(x1, y1, x2, y2);
+        System.out.println(dist);
+        System.out.println(arrayOfRabbits.get(0).getViewDistance());
+
+        //When rabbits are in a specific distance from each other we fill the ArrayList with a new Rabbit.
+        if (dist < arrayOfRabbits.get(0).getViewDistance() && dist > arrayOfRabbits.get(0).getViewDistance()-3) {
+            getArrayOfRabbits(this, 10, 10);
+
+        }
+    }
 }
--- src/Rabbit.java
+++ src/Rabbit.java
@@ -6,15 +6,19 @@ import java.util.concurrent.TimeUnit;
 
 public class Rabbit {
 
+
     PApplet p;
     private int x = 0;
     private int y = 0;
+    int currentX;
+    int currentY;
+    public int matingCondition = 0;
+    private int viewDistance = 100;
     private int movementSpeed = 3;
     public Random randomNumber = new Random();
     int newX = randomNumber.nextInt(500);
     int newY = randomNumber.nextInt(500);
-
-
+    int timeUntilMature = 10000;
 
 
     public Rabbit(PApplet p, int x, int y) // Contructor
@@ -23,20 +27,26 @@ public class Rabbit {
         this.setY(y);
         this.p = p;
 
+        int savedTime = p.millis();
+        int passedTime = p.millis() - savedTime;
+
+        if (passedTime > timeUntilMature)
+        {
+            matingCondition = 1;
+        }
+
     }
 
     public void drawRabbit()  // Graphical representation
     {
         p.rectMode(PConstants.CENTER);
         p.fill(255);
-        p.rect(getX(), getY(),10,10);
+        p.rect(getX(), getY(), 10, 10);
         p.ellipseMode(PConstants.CENTER);
         p.noFill();
-        p.ellipse(getX(),getY(),100,100);
-
-        //moveDirection();
+        p.ellipse(getX(), getY(), viewDistance, viewDistance);
         rabbitMovement();
-
+        //moveDirection();
         try {
             TimeUnit.MILLISECONDS.sleep(10);
         } catch (InterruptedException e) {
@@ -44,66 +54,59 @@ public class Rabbit {
         }
     }
 
-    public void rabbitMovement()
-    {
-        int currentX = this.x;
-        int currentY = this.y;
+    public void rabbitMovement() {
+        currentX = this.x;
+        currentY = this.y;
         //if (currentX == newX && currentY == newY)
-        if (currentX < newX-5 || currentX > newX+5 && currentY < newY+5 || currentY > newY-5)
-        {
+        if (currentX < newX - 5 || currentX > newX + 5 && currentY < newY + 5 || currentY > newY - 5) {
             this.newX = randomNumber.nextInt(p.width);
             this.newY = randomNumber.nextInt(p.height);
         }
 
-        if (currentX < newX)
-        {
-            x = x + movementSpeed;
+
+            if (currentX < newX) {
+                x = x + movementSpeed;
+            }
+            if (currentX > newX) {
+                x = x - movementSpeed;
+            }
+            if (currentY < newY) {
+                y = y + movementSpeed;
+            }
+            if (currentY > newY) {
+                y = y - movementSpeed;
+            }
         }
-        if (currentX > newX)
-        {
-            x = x - movementSpeed;
-        }
-        if (currentY < newY)
-        {
-            y = y + movementSpeed;
-        }
-        if (currentY > newY)
-        {
-            y = y - movementSpeed;
-        }
-    }
 
     public void moveDirection() {
 
-        int a =randomNumber.nextInt(5);
+        int a = randomNumber.nextInt(5);
 
-        switch (a){
+        switch (a) {
 
             case 0:
-                if(x < p.width){
+                if (x < p.width) {
                     this.x += movementSpeed;
                 }
                 break;
             case 1:
-                if ( x > 0){
+                if (x > 0) {
                     this.x -= movementSpeed;
                 }
                 break;
             case 2:
-                if (y < p.height -3){
+                if (y < p.height - 3) {
                     this.y += movementSpeed;
                 }
                 break;
             case 3:
-                if (y > 3 ){
+                if (y > 3) {
                     this.y -= movementSpeed;
                 }
                 break;
         }
 
     }
-
-
 
     public int getX() {
         return x;
@@ -119,5 +122,22 @@ public class Rabbit {
 
     public void setY(int y) {
         this.y = y;
+    }
+
+    public void setNewX(int newX) {
+        this.newX = newX;
+    }
+
+    public void setNewY(int newY) {
+        this.newX = newX;
+    }
+
+
+    public int getViewDistance() {
+        return viewDistance;
+    }
+
+    public void setmatingCondition(int matingCondition) {
+        this.matingCondition = matingCondition;
     }
 }
