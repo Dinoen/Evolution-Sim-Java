597984a0e31bd55cfb3e837a8c66e65043db5f67
Ec2IsDifficult <rasser.arm@live.dk>
2019-12-10 09:53:08 +0100

    Getting the newest

--- src/GraphingTheData.java
+++ src/GraphingTheData.java
@@ -1,17 +1,9 @@
 import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.scene.chart.LineChart;
-import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
-import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 import javafx.scene.*;
 import processing.core.PApplet;
-
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 public class GraphingTheData extends Application {
 
@@ -21,11 +13,10 @@ public class GraphingTheData extends Application {
     Scene scene1, scene2;
 
     //creating an XYCHART that is called data
-    XYChart.Series<Number, Number> data;
     XYChart.Series<Number, Number> data1;
     //creating the ScheduledExecutorService
-    private ScheduledExecutorService scheduledExecutorService;
-    private ScheduledExecutorService scheduledExecutorService1;
+    createGraph graph1;
+    createGraph graph2;
 
     public static void main(String[] args) {
         launch(args);
@@ -42,8 +33,10 @@ public class GraphingTheData extends Application {
     private void init(Stage primaryStage) {
 
         window = primaryStage;
-        HBox root = new HBox();
-        scene2 = new Scene(root, 450, 330);
+        graph1 = new createGraph();
+        graph2 = new createGraph();
+        scene2 = new Scene(graph1.root, 450, 330);
+        scene1 = new Scene(graph2.root, 450, 330);
 
         window.setScene(scene2);
 
@@ -51,112 +44,20 @@ public class GraphingTheData extends Application {
         button2.setOnAction(e -> window.close());
         button2.setOnAction(e -> window.setScene(scene2));
 
-
-        //Creating two numberAxis x and y. These are animated and the are labelt
-        NumberAxis xAxis = new NumberAxis();
-        xAxis.setLabel("Time");
-        xAxis.setAnimated(true);
-
-        NumberAxis yAxis = new NumberAxis();
-        yAxis.setLabel("Average Of Rabbit Speed Genes");
-        yAxis.setAnimated(true);
-
-        //Creating our linechart and setting the title
-        LineChart<Number, Number> lineChart = new LineChart<Number, Number>(xAxis, yAxis);
-        lineChart.setTitle("Graph of Data");
-
-        //initializing the data XYChart
-        data = new XYChart.Series<>();
-
-        //feeding the lineChart the XYChart called data. Providing the
-        lineChart.getData().add(data);
-        lineChart.setAnimated(true);
-        root.getChildren().add(lineChart);
-
-
-        scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
-        scheduledExecutorService.scheduleAtFixedRate(() -> {
-            //here we are sending the data to a que that runsLater or runs when it it supposed to.
-            Platform.runLater(() -> {
-                data.getData().add(new XYChart.Data<Number, Number>(returnMillisecSinceBeginning(), getTheAverageSpeedGene()));
-            });
-        }, 0, 1, TimeUnit.SECONDS);
-
-        //rabbitPopulationSize();
-        //sequentially returning a task object, and we are using fixed rate
-
-        HBox root1 = new HBox();
-        Scene scene1 = new Scene(root1,450,330);
-
         Button button1 = new Button("Go to populationSize");
         button1.setOnAction(e -> window.close());
         button1.setOnAction(e -> window.setScene(scene1));
-        root.getChildren().addAll(button1);
-        root1.getChildren().addAll(button2);
+        graph1.root.getChildren().addAll(button1);
+        graph2.root.getChildren().addAll(button2);
 
-        NumberAxis xAxis1 = new NumberAxis();
-        xAxis1.setLabel("Time");
-        xAxis1.setAnimated(true);
+        graph1.scheduledExecutorService();
+        graph1.getTheAverageSpeedGene();
+        graph1.returnMillisecSinceBeginning();
 
-        NumberAxis yAxis1 = new NumberAxis();
-        yAxis1.setLabel("The Overall Population");
-        yAxis1.setAnimated(true);
-
-        //Creating our linechart and setting the title
-        LineChart<Number, Number> lineChart1 = new LineChart<Number, Number>(xAxis1, yAxis1);
-        lineChart1.setTitle("Graph of Data");
-
-        //initializing the data XYChart
-        data1 = new XYChart.Series<>();
-
-        lineChart1.getData().add(data1);
-        lineChart1.setAnimated(true);
-        root1.getChildren().add(lineChart1);
-
-
-        scheduledExecutorService1 = Executors.newSingleThreadScheduledExecutor();
-        scheduledExecutorService1.scheduleAtFixedRate(() -> {
-            //here we are sending the data to a que that runsLater or runs when it it supposed to.
-            Platform.runLater(() -> {
-                data1.getData().add(new XYChart.Data<Number, Number>(returnMillisecSinceBeginning(), rabbitPopulationSize()));
-            });
-        }, 0, 1, TimeUnit.SECONDS);
+        graph2.scheduledExecutorService1();
+        graph2.rabbitPopulationSize();
+        graph2.returnMillisecSinceBeginning();
 
         window.show();
-    }
-
-
-    public int rabbitPopulationSize() {
-
-        int theRabbitPopulation = 0;
-
-        for (int i = 0; i < Main.allEntities.get(0).arrayOfEntities.size(); i++) {
-            if(Main.allEntities.get(0).arrayOfEntities.size() != 0) {
-                theRabbitPopulation = Main.allEntities.get(0).arrayOfEntities.size();
-            }
-        }
-
-        return theRabbitPopulation;
-    }
-
-    //we return and int that is the poopulation size of the rabbits
-    public float getTheAverageSpeedGene() {
-        float amount = 0;
-        float theSpeedGenesAverage;
-
-
-        for (int i = 0; i < Main.allEntities.get(0).getEntities().size(); i++) {
-            if (Main.allEntities.get(0).getEntities().get(i) != null) {
-                amount = amount+ Main.allEntities.get(0).getEntities().get(i).movementSpeed;
-            }
-        }
-        theSpeedGenesAverage = amount / Main.allEntities.get(0).getEntities().size();
-        return theSpeedGenesAverage;
-    }
-
-    //returning the time since the program has started
-    public int returnMillisecSinceBeginning() {
-        int theTimeInMinutes = (p.millis() / 1000);
-        return theTimeInMinutes;
     }
 }
---
+++ src/createGraph.java
@@ -0,0 +1,100 @@
+import javafx.application.Platform;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.layout.HBox;
+import processing.core.PApplet;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class createGraph {
+
+    PApplet p = new PApplet();
+    XYChart.Series<Number, Number> data;
+    HBox root;
+    NumberAxis xAxis;
+    NumberAxis yAxis;
+    LineChart<Number, Number> lineChart;
+    private ScheduledExecutorService scheduledExecutorService;
+    private ScheduledExecutorService scheduledExecutorService1;
+
+
+    createGraph() {
+
+        root = new HBox(1000);
+        xAxis = new NumberAxis();
+        xAxis.setLabel("Time");
+        xAxis.setAnimated(true);
+
+        yAxis = new NumberAxis();
+        yAxis.setLabel("Average Of Rabbit Speed Genes");
+        yAxis.setAnimated(true);
+
+        //Creating our linechart and setting the title
+        lineChart = new LineChart<Number, Number>(xAxis, yAxis);
+        lineChart.setTitle("Graph of Data");
+
+        //initializing the data XYChart
+        data = new XYChart.Series<>();
+
+        //feeding the lineChart the XYChart called data. Providing the
+        lineChart.getData().add(data);
+        lineChart.setAnimated(true);
+        root.getChildren().add(lineChart);
+    }
+
+    public void scheduledExecutorService() {
+        scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+        scheduledExecutorService.scheduleAtFixedRate(() -> {
+            //here we are sending the data to a que that runsLater or runs when it it supposed to.
+            Platform.runLater(() -> {
+                data.getData().add(new XYChart.Data<Number, Number>(returnMillisecSinceBeginning(), getTheAverageSpeedGene()));
+            });
+        }, 0, 1, TimeUnit.SECONDS);
+    }
+
+    public void scheduledExecutorService1() {
+        scheduledExecutorService1 = Executors.newSingleThreadScheduledExecutor();
+        scheduledExecutorService1.scheduleAtFixedRate(() -> {
+            //here we are sending the data to a que that runsLater or runs when it it supposed to.
+            Platform.runLater(() -> {
+                data.getData().add(new XYChart.Data<Number, Number>(returnMillisecSinceBeginning(), rabbitPopulationSize()));
+            });
+        }, 0, 1, TimeUnit.SECONDS);
+    }
+
+
+    public int returnMillisecSinceBeginning() {
+        int theTimeInMinutes = (p.millis() / 1000);
+        return theTimeInMinutes;
+    }
+
+    public float getTheAverageSpeedGene() {
+        float amount = 0;
+        float theSpeedGenesAverage;
+
+
+        for (int i = 0; i < Main.allEntities.get(0).getEntities().size(); i++) {
+            if (Main.allEntities.get(0).getEntities().get(i) != null) {
+                amount = amount + Main.allEntities.get(0).getEntities().get(i).movementSpeed;
+            }
+        }
+        theSpeedGenesAverage = amount / Main.allEntities.get(0).getEntities().size();
+        return theSpeedGenesAverage;
+    }
+
+    public int rabbitPopulationSize() {
+
+        int theRabbitPopulation = 0;
+
+        for (int i = 0; i < Main.allEntities.get(0).arrayOfEntities.size(); i++) {
+            if (Main.allEntities.get(0).arrayOfEntities.size() != 0) {
+                theRabbitPopulation = Main.allEntities.get(0).arrayOfEntities.size();
+            }
+        }
+
+        return theRabbitPopulation;
+    }
+}
